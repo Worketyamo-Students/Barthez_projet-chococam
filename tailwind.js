16e4c5d7f71605b01632cc83308ba0ec19c4e91c
Your Name <you@example.com>
2024-06-04 21:08:35 +0100

    premiere version du projet chococam

---
+++ tailwind.js
@@ -0,0 +1,38 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'marron-50': '#3E3232',
+                'marron-300': '#3E3232',
+                'marron-400': '#020202',
+                'gray-300': '#DFCECE',
+            },
+
+            fontFamily: {
+                'body': ["Montserrat", "sans-serif"],
+            },    
+
+            padding: {
+                '18': '70px',
+            },
+
+            fontSize: {
+                '5.5px': '55px',
+            },
+
+            gap: {
+                '18': '70px',
+            },
+
+            boxShadow: {
+                'text-2d': '0px 4px 4px 0px #00000040',
+            },
+        }, 
+
+
+    }, 
+
+    plugins: [],
+  }
+
